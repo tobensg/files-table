14c4279ec0657912be032185242e83f02d7747d1
Toben Green <greent@TSG-Autodesk-WorkBook-Pro.local>
2022-09-11 20:50:20 -0700

    remove testing messages (did not do any) and add empty state

--- src/files-list/table-body.js
+++ src/files-list/table-body.js
@@ -1,13 +1,19 @@
 import TableRow from './table-row'
 
-const TableBody = ({toggleRow, localData}) => (
-	<tbody>
-		{localData && localData.map( (row, index) => {
-			return (
-				<TableRow key={`row${index}`} row={row} index={index} toggleRow={toggleRow} />
-			)
-		})}
-	</tbody>
-)
+const TableBody = ({toggleRow, localData}) => {
+	if (localData.length === 0) {
+		// empty state handling
+		return (<tbody><tr><td colSpan="5"><center>No files to display</center></td></tr></tbody>)
+	}
+	return (
+		<tbody>
+			{localData && localData.map( (row, index) => {
+				return (
+					<TableRow key={`row${index}`} row={row} index={index} toggleRow={toggleRow} />
+				)
+			})}
+		</tbody>
+	)
+}
 
 export default TableBody
